1baf9495e3f9f34c81dc816ec972662b3eed0204
Udaya <udaya.vepakomma@nike.com>
2020-01-14 23:33:17 -0800

    Added JSON Spark reads

--- spark-s3.py
+++ spark-s3.py
@@ -1,5 +1,6 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import Row, SQLContext
+from pyspark.sql import SparkSession
 conf = SparkConf()
 sc = SparkContext(conf = conf)
 sqlContext = SQLContext(sc)
@@ -8,16 +9,10 @@ spark = SparkSession.builder.appName("av_sf").getOrCreate()
 
 #s3://lpm-line-plan-domain-data-preprod/av/channel.json
 
-av_df = spark.read.csv("s3a://lpm-line-plan-domain-data-preprod/av/channel.json",header=False)
-csv_data = av_df.map(lambda l: l.split("\n"))
-row_data = csv_data.map(lambda p: Row(
-    avProductId=p["avProductId"], 
-    seasonYear_abbrn=p["seasonYearAbbreviation"]
-    )
-)
+av_df = spark.read.json("s3a://lpm-line-plan-domain-data-preprod/raw/stg/assort_visual_cluster.json")
 
-av_parsed_df = sqlContext.createDataFrame(row_data)
-av_parsed_df.registerTempTable("av_df")
+av_df.registerTempTable("av_df")
 
-av_df_sql = sqlContext.sql("select avProductId from av_parsed_df ")
+av_df_sql = sqlContext.sql("select key_id, explode(newValue) as new_value  from av_df  ")
 av_df_sql.show()
+av_df.printSchema()
